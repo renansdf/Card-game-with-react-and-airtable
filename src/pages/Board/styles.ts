96969a49365f5fcda285fa9f2ab605c12b77550c
renansdf <rnn.frts@gmail.com>
2020-11-06 17:40:28 -0300

    added styles for csv container

--- src/pages/Board/styles.ts
+++ src/pages/Board/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import trianguloCapa from '../../images/capa-bg.svg';
 import trianguloMetade from '../../images/meio-triangulo-direita.svg';
+
+interface ICSVContainerProps {
+  isVisible: boolean;
+}
 
 export const Container = styled.div`
   display: flex;
@@ -15,4 +19,54 @@ export const Container = styled.div`
   background-repeat: no-repeat, no-repeat;
   background-position: top left, bottom right;
   color: #3405A2;
+`;
+
+export const CSVContainer = styled.div<ICSVContainerProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #3405A2;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  section{
+    width: 400px;
+    position: relative;
+  }
+
+  h1{
+    color: #fff;
+    font-weight: 300;
+    font-size: 40px;
+  }
+
+  p{
+    color: #fff;
+  }
+
+  label{
+    color: #3405A2;
+    background: #fff;
+    position: absolute;
+    left: 20px;
+    bottom: 35px;
+    padding: 2px 15px;
+    border-radius: 30px;
+  }
+
+  p + p{
+    margin-top: 10px;
+  }
+
+  div{
+    margin-top: 30px;
+  }
+
+  ${props => props.isVisible && css`
+    display: none;
+  `}
 `;
