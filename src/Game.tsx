7bbfb9cd55623b3bb75f9643b50bf095e8043616
renansdf <rnn.frts@gmail.com>
2020-11-09 22:25:47 -0300

    added deck provider

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import Board from './pages/Board';
+import { DeckProvider } from './hooks/Deck';
 
 const Game: React.FC = () => {
   return (
-    <>
+    <DeckProvider>
       <Board />
       <GlobalStyle />
-    </>
+    </DeckProvider>
   );
 };
 
